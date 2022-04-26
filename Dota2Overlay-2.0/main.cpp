c8cbef2825686146f6054b47c783f9027c626437
Martin S <35738176+skrixx68@users.noreply.github.com>
2022-04-26 16:05:53 +0800

    changed rendering api to Direct2d

--- Dota2Overlay-2.0/main.cpp
+++ Dota2Overlay-2.0/main.cpp
@@ -1,21 +1,17 @@
-#include "hMain.h"
+#include "DirectOverlay.h"
+#include "include.h"
 #include "Dota2MemReader.h"
 
-HWND hWnd, TargetWnd;
-MSG Message;
-RECT WindowRect, ClientRect;
 int windowWidth, windowHeight, overlayScale, overlayX_Pos, overlayY_Pos;
-int clientWidth = 1920, clientHeight = 1080;
-int borderWidth, borderHeight; 
-int savedX_Pos,savedY_Pos,savedFont_Size;
-
-char lWindowName[256] = "GG";
+float r = 1;
+float g = 0;
+float b = 0;
+float a = 1;
+int fontSize = 50;
+bool init = false;
+HWND tWindow;
 char tWindowName[256] = "Dota 2";
-
-const MARGINS pMargin = { 0,0, clientWidth, clientHeight };
-
-
-
+char hWindowName[256] = "Dota 2";
 
 int getPercent(float percent, float value)
 {
@@ -25,26 +21,18 @@ int getPercent(float percent, float value)
 // Restore previous position and fontsize from Config
 void getConfig() {
 	std::string line;
-	std::ifstream config("overlay.conf");
+	std::ifstream config("overlay.ini");
 	if (config.is_open()) {
 		if (config.peek())
 		{
-			OutputDebugString(L"Config loading..\n");
-			std::getline(config, line);
-			overlayX_Pos = std::stoi(line);
-			_RPT1(0, "X = %u\n", overlayX_Pos);
+			std::cout << "Config loading ..." << std::endl;
+			std::getline(config, line); if (true)
+				overlayX_Pos = std::stoi(line);
 			std::getline(config, line);
 			overlayY_Pos = std::stoi(line);
-			_RPT1(0, "Y = %u\n", overlayY_Pos);
 			std::getline(config, line);
 			fontSize = std::stoi(line);
-			_RPT1(0, "Font = %u\n", fontSize);
-			setFontSize(fontSize);
 		}
-	}
-	else {
-		overlayX_Pos = getPercent(overlayScale, clientWidth);
-		overlayY_Pos = getPercent(4, clientHeight);
 	}
 	config.close();
 
@@ -52,7 +40,7 @@ void getConfig() {
 
 void setConfig() {
 	std::fstream config;
-	config.open("overlay.conf", std::ios::out | std::ios::out);
+	config.open("overlay.ini", std::ios::out | std::ios::out);
 	if (config.is_open())
 	{
 		config << overlayX_Pos << std::endl;
@@ -60,176 +48,82 @@ void setConfig() {
 		config << fontSize << std::endl;
 
 	}
-	else {
-		OutputDebugString(L"Error Saving Config");
-	}
 	config.close();
 }
 
-LRESULT CALLBACK Proc(HWND hWnd, UINT Message, WPARAM wParam, LPARAM lParam)
-{
-	switch (Message)
+
+
+void drawLoop(int width, int height) {
+
+	// if insert is pressed we move our TextOverlay to current cursor position
+	if (GetAsyncKeyState(VK_INSERT) & 1)
 	{
-
-	case WM_PAINT:
-
-
-		if (TargetWnd == GetForegroundWindow())
+		POINT p;
+		if (GetCursorPos(&p))
 		{
-			dx_Device->Clear(0, 0, D3DCLEAR_TARGET, 0, 1.0f, 0);
-			dx_Device->BeginScene();
-			Render();
-			dx_Device->EndScene();
-			dx_Device->PresentEx(0, 0, 0, 0, 0);
-		}
-		else
-		{
-			dx_Device->Clear(0, 0, D3DCLEAR_TARGET, 0, 1.0f, 0);
-			dx_Device->BeginScene();
+			overlayX_Pos = p.x;
+			overlayY_Pos = p.y;
+			setConfig();
 
-			dx_Device->EndScene();
-			dx_Device->PresentEx(0, 0, 0, 0, 0);
 		}
 
-		break;
-	case WM_CREATE:
-		return DwmExtendFrameIntoClientArea(hWnd, &pMargin); 
-		break;
-	case WM_DESTROY:
-		PostQuitMessage(0); 
-		break;
-	default:
-		return DefWindowProc(hWnd, Message, wParam, lParam); 
-		break;
 	}
+	// if PageUp is pressed we increase font size
+	if (GetAsyncKeyState(VK_PRIOR) & 1)
+	{
+		fontSize += 2;
+		setConfig();
+	}
+	// if PageDown is pressed we decrease font size
+	else if (GetAsyncKeyState(VK_NEXT) & 1)
+	{
+		fontSize -= 2;
+		setConfig();
+	}
+
+	if (tWindow == GetForegroundWindow())
+	{
+		int vbe = getVbe();
+		if (vbe == 14)
+		{
+			DrawString("Visible", fontSize, overlayX_Pos, overlayY_Pos, r, g, b, a);
+		}
+		else if (vbe <= 0)
+		{
+			DrawString("Waiting", fontSize, overlayX_Pos, overlayY_Pos, 125, 125, 125);
+		}
+
+	}
+	Sleep(100);
 }
 
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+void main()
 {
-	WNDCLASSEXA OverlayWnd; // contains window class information
-	OverlayWnd.cbSize = sizeof(WNDCLASSEXA); // size of struct, basically checking for version or check
-	OverlayWnd.style = CS_HREDRAW | CS_VREDRAW;  // Style, redraw method type
-	OverlayWnd.lpfnWndProc = Proc; // Pointer to the window procedure
-	OverlayWnd.cbClsExtra = 0; // window class struct extra bytes
-	OverlayWnd.cbWndExtra = 0; // window instance extra bytes
-	OverlayWnd.hInstance = hInstance; // handle to the instance that contains the window procedure for the class
-	OverlayWnd.hIcon = LoadIcon(NULL, IDI_APPLICATION); // basic window icon set
-	OverlayWnd.hIconSm = LoadIcon(NULL, IDI_APPLICATION); // basic window icon set
-	OverlayWnd.hCursor = LoadCursor(NULL, IDC_ARROW); // basic window cursor icon set
-	OverlayWnd.hbrBackground = (HBRUSH)CreateSolidBrush(RGB(0, 0, 0)); // handle to the class background brush
-	OverlayWnd.lpszMenuName = lWindowName;
-	OverlayWnd.lpszClassName = lWindowName;
-
-	if (!RegisterClassExA(&OverlayWnd))
-		exit(1);
-
-	TargetWnd = FindWindowA(0, tWindowName);
-
-
-	if (TargetWnd)
+	// Window
+	std::cout << "Waiting for dota2.exe:\t";
+	tWindow = FindWindowA(0, tWindowName);
+	if (tWindow == 0)
 	{
-		overlayScale = 45;
-		GetWindowRect(TargetWnd, &WindowRect);
-		windowWidth = WindowRect.right - WindowRect.left;
-		windowHeight = WindowRect.bottom - WindowRect.top;
-		clientHeight = windowHeight;
-		clientWidth = windowWidth;
-		hWnd = CreateWindowExA(WS_EX_TOPMOST | WS_EX_TRANSPARENT | WS_EX_LAYERED, lWindowName, lWindowName, WS_POPUP, 1, 1, windowWidth, windowHeight, 0, 0, 0, 0);
-		SetLayeredWindowAttributes(hWnd, RGB(0, 0, 0), 0, LWA_COLORKEY);
-		SetLayeredWindowAttributes(hWnd, 0, 255, LWA_ALPHA);
-		ShowWindow(hWnd, SW_SHOW);
+		std::cout << "Waiting..";
+		while (true)
+		{
+			Sleep(1000);
+			tWindow = FindWindowA(0, tWindowName);
+			if (tWindow != 0) { break; }
+		}
 	}
+	std::cout << "Game found ..." << std::endl;
 
-	if (initHack() == -1)
-	{
-		MessageBox(NULL, L"Overlay failed to initialize...", L"Error", MB_ICONERROR);
-		exit(1);
-	}
-	
-
-	bool init = false;
-
-	D3D9Init(hWnd);
-
-	//While Delete key is not pressed, we will be enable our hack.
-	while (!GetAsyncKeyState(VK_DELETE))
-	{
-		if (PeekMessage(&Message, hWnd, 0, 0, PM_REMOVE))
-		{
-			TranslateMessage(&Message);
-			DispatchMessage(&Message);
-		}
-
-		TargetWnd = FindWindowA(0, tWindowName);
-
-		if (!TargetWnd)
-			exit(1);
-
-		GetWindowRect(TargetWnd, &WindowRect);
-		GetClientRect(TargetWnd, &ClientRect);
-
-		windowWidth = WindowRect.right - WindowRect.left;
-		windowHeight = WindowRect.bottom - WindowRect.top;
-		DWORD dwStyle = GetWindowLong(TargetWnd, GWL_STYLE);
-
-		if (dwStyle & WS_BORDER)
-		{
-
-			windowHeight = WindowRect.bottom - WindowRect.top;
-			windowWidth = WindowRect.right - WindowRect.left;
-			clientHeight = ClientRect.bottom - ClientRect.top;
-			clientWidth = ClientRect.right - ClientRect.left;
-			borderHeight = (windowHeight - ClientRect.bottom);
-			borderWidth = (windowWidth - ClientRect.right) / 2; //only want one side
-			borderHeight -= borderWidth; //remove bottom from width, bottom is the same size as either side so we subtract that
-
-			WindowRect.left += borderWidth;
-			WindowRect.top += borderHeight;
-			_RPT1(0, "Window He = %u", windowHeight);
-
-		}
-
-		
-		if (init==false)
-		{
-			getConfig();
-			init = true;
-		}
-
-		// if insert is pressed we move our TextOverlay to current cursor position
-		if (GetAsyncKeyState(VK_INSERT) & 1)
-		{
-			POINT p;					
-			if (GetCursorPos(&p))
-			{
-				if (ScreenToClient(hWnd, &p))
-				{
-					overlayX_Pos = p.x;
-					overlayY_Pos = p.y;
-					setConfig();
-				}
-			}
-			
-		}
-		// if PageUp is pressed we increase font size
-		if (GetAsyncKeyState(VK_PRIOR) & 1)
-		{
-			setFontSize(fontSize += 2);
-			setConfig();
-		}
-		// if PageDown is pressed we deccrease font size
-		else if (GetAsyncKeyState(VK_NEXT) & 1)
-		{
-			setFontSize(fontSize -= 2);
-			setConfig();
-		}
-		MoveWindow(hWnd, WindowRect.left, WindowRect.top, clientWidth, clientHeight, true);
-
-	} 
-	setConfig();
-
-	close();
-	exit(0);
-
+	std::cout << "Initializing ..." << std::endl;
+	initHack(); 
+	getConfig();
+	DirectOverlaySetOption(D2DOV_FONT_IMPACT | D2DOV_REQUIRE_FOREGROUND);
+	DirectOverlaySetup(drawLoop, tWindow);
+	std::cout << "Done ..." << std::endl ;
+	std::cout << "\nHotkeys :"<< std::endl;
+	std::cout << "INSERT = Move Overlay to current position of cursor" << std::endl;
+	std::cout << "PAGE_UP = Increase Font Size" << std::endl;
+	std::cout << "PAGE_DOWN = Decrease Font Size" << std::endl;
+	std::cout << "\nPress enter to exit... \t" << std::endl;
+	system("pause");
 }
